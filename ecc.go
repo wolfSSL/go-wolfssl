98b9bc24f7dde508c88cc864b173f4f5a7e047d7
Lealem Amedie <lealem@wolfssl.com>
2025-06-25 14:11:45 -0600

    Copyright 2025 update

--- ecc.go
+++ ecc.go
@@ -1,6 +1,6 @@
 /* ecc.go
  *
- * Copyright (C) 2006-2022 wolfSSL Inc.
+ * Copyright (C) 2006-2025 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
--- ssl.go
+++ ssl.go
@@ -1,6 +1,6 @@
 /* ssl.go
  *
- * Copyright (C) 2006-2022 wolfSSL Inc.
+ * Copyright (C) 2006-2025 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
